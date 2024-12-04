755859a021949e256a8884ed8c39ecbe06d54a0a
Marco Ferreira <marcof@br.ibm.com>
2024-12-04 17:15:17 -0300

    [minor] implemented worker

--- app/src/main/java/com/joselaine/hidratamais/MainActivity.kt
+++ app/src/main/java/com/joselaine/hidratamais/MainActivity.kt
@@ -12,22 +12,37 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import com.joselaine.hidratamais.ui.theme.BluePrimary
 import com.joselaine.hidratamais.ui.theme.BlueSecondary
 import com.joselaine.hidratamais.ui.theme.HidrataMaisTheme
+import java.util.UUID
+import java.util.concurrent.TimeUnit
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             HidrataMaisTheme {
+                val context = LocalContext.current
                 HidrataMaisApp {
+                    val workManager = WorkManager.getInstance(context)
+                    val repeatingRequest = PeriodicWorkRequestBuilder<NotificationWorker>(15, TimeUnit.SECONDS)
+                        .build()
+
+                    val workId = UUID.randomUUID().toString()
+
+                    workManager.enqueueUniquePeriodicWork(workId, ExistingPeriodicWorkPolicy.KEEP, repeatingRequest)
 
                 }
             }
